7d539b26735b38774ede4c0ac9802f6c64776e17
KyungsooLee <jmtkd9196@gmail.com>
2024-04-18 20:00:40 +0900

    Initial Commit

---
+++ GithubSessionPrjoect/GithubSessionPrjoectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSessionPrjoectApp.swift
+//  GithubSessionPrjoect
+//
+//  Created by kyungsoolee on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubSessionPrjoectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
